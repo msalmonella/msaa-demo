c8d9231344fbc349df902e6393f5f7e85e4212c0
salmonella <cahitatillab@gmail.com>
2026-06-28 23:38:56 +0300

    grid succesfully drawn on the screen

--- drawer.py
+++ drawer.py
@@ -9,10 +9,14 @@ renderer = definitions.Renderer()
 rect_values: list[int] = [0, 0, renderer.cell_size * 100, renderer.cell_size * 100]
 
 
+""" responsible for drawing grid on the screen. """
 def draw_grid(screen) -> None:
-    for _ in range(5):
-        pygame.draw.rect(screen, "blue", rect_values, 1)
-        rect_values[0] = rect_values[0] + renderer.cell_size * 100
-        for _ in range(5):
-            pygame.draw.rect(screen, "blue", rect_values, 1)
-            rect_values[1] = rect_values[1] + renderer.cell_size * 100
+    x_starting_value = rect_values[0]
+    y_starting_value = rect_values[1]
+    for _ in range(20):
+        for _ in range(20):
+            pygame.draw.rect(screen, "red", rect_values, 2)
+            rect_values[0] += 100
+        rect_values[0] = x_starting_value
+        rect_values[1] += 100
+    rect_values[1] = y_starting_value
